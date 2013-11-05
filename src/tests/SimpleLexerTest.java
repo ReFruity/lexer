f4165a83b8eeef3058be09bae534ebf4e242bc82
ReFruity <istandon8@gmail.com>
2013-11-05 13:52:59 +0600

    Added couple of small tests to the SimpleLexerTest and corresponding method to SimpleLexerTest.Tester

--- src/tests/SimpleLexerTest.java
+++ src/tests/SimpleLexerTest.java
@@ -2,6 +2,7 @@ package tests;
 
 import junit.framework.TestCase;
 import lexer.*;
+
 import java.util.ArrayList;
 
 public class SimpleLexerTest extends TestCase {
@@ -13,52 +14,71 @@ public class SimpleLexerTest extends TestCase {
      *	"ws" - whitespace
      *	"kw" - keyword from wordreader
      */
-    public void testSimpleLexer() {
+    // No exceptions
+    public void testIdReader() throws SimpleLexer.UnknownTokenException {
+        Tester idTester = new Tester(new IdentifierReader());
+        idTester.checkReadNextToken("qwe42+'", new Token("id", "qwe42"));
+    }
+
+    public void testIntReader() throws SimpleLexer.UnknownTokenException {
+        Tester intTester = new Tester(new IntReader());
+        intTester.checkReadNextToken("0X14 5'", new Token("i", "0X14", 20));
+    }
+
+    public void testWordReader() throws SimpleLexer.UnknownTokenException {
+        Tester kwTester = new Tester(new WordReader("keyword", false));
+        kwTester.checkReadNextToken("keywordandanotherword", new Token("kw", "keyword"));
+    }
+
+    public void testConcurrentReaders() throws SimpleLexer.UnknownTokenException {
+        Tester concurrentTester = new Tester(new WhitespaceReader(), new WordReader("identifier", false), new IdentifierReader());
+        concurrentTester.checkOutput("some identifier",
+                new Token("id", "some"), new Token("ws", " "),
+                new Token("id", "identifier")
+        );
+        concurrentTester = new Tester(new WhitespaceReader(), new IdentifierReader(), new WordReader("keyword", false));
+        concurrentTester.checkOutput("some keyword",
+                new Token("id", "some"), new Token("ws", " "),
+                new Token("kw", "keyword")
+        );
+    }
+
+    public void testSimpleLexer() throws SimpleLexer.UnknownTokenException {
         Tester legitTester = new Tester(
                 new IdentifierReader(), new IntReader(), new WhitespaceReader(),
                 new WordReader("word", false), new WordReader("butanumber", true)
         );
+        legitTester.checkOutput("0x42 isnta word ButANumber 6_6L",
+                new Token("i", "0x42", 66), new Token("ws", " "),
+                new Token("id", "isnta"), new Token("ws", " "),
+                new Token("kw", "word"), new Token("ws", " "),
+                new Token("kw", "ButANumber"), new Token("ws", " "),
+                new Token("l", "6_6L", 66L)
+        );
+    }
+    // With exceptions
+    public void testEmptyString() {
+        Tester emptyTester = new Tester(new WhitespaceReader());
         try {
-            legitTester.checkOutput("0x42 isnta word ButANumber 6_6L",
-                    new Token("i", "0x42", 66), new Token("ws", " "),
-                    new Token("id", "isnta"), new Token("ws", " "),
-                    new Token("kw", "word"), new Token("ws", " "),
-                    new Token("kw", "ButANumber"), new Token("ws", " "),
-                    new Token("l", "6_6L", 66L)
-            );
-
-            legitTester.checkOutput("");
+            emptyTester.checkReadNextToken("", new Token("error", "wrong token"));
+            fail("Empty string did not invoke any exceptions.");
         }
         catch (SimpleLexer.UnknownTokenException ex) {
-              fail();
+            assertEquals("", ex.where);
         }
     }
 
     public void testExceptions() {
         Tester exTester = new Tester(new IntReader(), new WhitespaceReader());
-        Tester exTester2 = new Tester(new IdentifierReader(), new WhitespaceReader(), new IntReader());
         try {
-            exTester.checkOutput("05 abc",
-                    new Token("i", "05", 5), new Token("ws", " "),
+            exTester.checkOutput("05\tabc",
+                    new Token("i", "05", 5), new Token("ws", "\t"),
                     new Token("id", "abc")
             );
-
-            fail();
+            fail("Exception tester did not invoke any exceptions.");
         }
         catch (SimpleLexer.UnknownTokenException ex) {
             assertEquals("abc", ex.where);
-        }
-        try {
-            exTester2.checkOutput("  Id42 0B1 +",
-                    new Token("ws", " "), new Token("ws", " "),
-                    new Token("id", "Id42"), new Token("ws", " "),
-                    new Token("i", "0B1", 1), new Token("ws", " ")
-            );
-
-            fail();
-        }
-        catch (SimpleLexer.UnknownTokenException ex) {
-            assertEquals("+", ex.where);
         }
     }
 
@@ -70,7 +90,8 @@ public class SimpleLexerTest extends TestCase {
             this.tokenReadersArray = tokenReadersArray;
         }
 
-        private void checkOutput(String input, Token... expectedTokensArray) throws SimpleLexer.UnknownTokenException {
+        private void checkOutput(String input, Token... expectedTokensArray)
+                                                throws SimpleLexer.UnknownTokenException {
             lexer = new SimpleLexer(input, tokenReadersArray);
             ArrayList<Token> readTokensList = new ArrayList<Token>();
             while (lexer.hasNextTokens())
@@ -80,6 +101,12 @@ public class SimpleLexerTest extends TestCase {
             readTokensList.toArray(readTokensArray);
             for (int i = 0; i < expectedTokensArray.length; i++)
                 assertEquals(expectedTokensArray[i], readTokensArray[i]);
+        }
+
+        private void checkReadNextToken(String input, Token expectedToken)
+                                                throws SimpleLexer.UnknownTokenException {
+            lexer = new SimpleLexer(input, tokenReadersArray);
+            assertEquals(expectedToken, lexer.readNextToken());
         }
     }
 }
