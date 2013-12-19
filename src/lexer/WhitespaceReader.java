04639c48d6e786e07adef12b5b9925de31772631
ReFruity <istandon8@gmail.com>
2013-12-19 17:42:50 +0600

    reading multiple whitespaces in one token

--- src/lexer/WhitespaceReader.java
+++ src/lexer/WhitespaceReader.java
@@ -4,7 +4,11 @@ public class WhitespaceReader extends TokenReader {
     public Token tryReadToken(String input, int offset) {
         if (input.length() == offset || !Character.isWhitespace(input.charAt(offset))) 
             return null;
-        else
-            return new Token("ws", input.substring(offset, offset + 1));
+        else {
+            int i = offset + 1;
+            while(i < input.length() && Character.isWhitespace(input.charAt(i)))
+                i++;
+            return new Token("ws", input.substring(offset, i));
+        }
     }
 }
